c100e117d78d212936fad73d8d4fc5653ce74189
matteociaroni <matteo.ciaroni@virgilio.it>
2022-06-11 20:19:30 +0200

    Added comments to ChatStatus

--- src/main/java/listthingsbot/telegrambot/ChatStatus.java
+++ src/main/java/listthingsbot/telegrambot/ChatStatus.java
@@ -10,5 +10,29 @@ import java.io.Serializable;
  */
 public enum ChatStatus implements Serializable
 {
-    DEFAULT, ADD_LIST, RENAME_LIST, ADD_ITEM, DELETE_ITEM
+    /**
+     * The default status
+     */
+    DEFAULT,
+
+    /**
+     * The user sent the /newlist command.
+     * The bot is waiting for the list title
+     */
+    ADD_LIST,
+
+    /**
+     * The bot is waiting for the new list title
+     */
+    RENAME_LIST,
+
+    /**
+     * The bot is waiting for the item to add to the list
+     */
+    ADD_ITEM,
+
+    /**
+     * The bot is waiting for the item to remove from the list
+     */
+    DELETE_ITEM
 }
