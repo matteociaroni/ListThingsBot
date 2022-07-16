62e222256e207257c484a97c1783b20834fb951b
matteociaroni <matteo.ciaroni@virgilio.it>
2022-07-16 09:43:28 +0200

    Changed helpMessage StringBuilder to concatenated String

--- src/main/java/listthingsbot/telegrambot/actions/commands/HelpCommand.java
+++ src/main/java/listthingsbot/telegrambot/actions/commands/HelpCommand.java
@@ -3,6 +3,12 @@ package listthingsbot.telegrambot.actions.commands;
 import listthingsbot.telegrambot.Chat;
 import listthingsbot.telegrambot.ChatStatus;
 
+/**
+ * This class represents the /help command.
+ * This command sends a message to the user with some information about the bot and its commands.
+ *
+ * @author Matteo Ciaroni
+ */
 public class HelpCommand extends Command
 {
 	public HelpCommand(Chat c)
@@ -15,12 +21,11 @@ public class HelpCommand extends Command
 	public void execute()
 	{
 		chat.status = ChatStatus.DEFAULT;
-		StringBuilder helpMessage = new StringBuilder();
-		helpMessage.append("This bot can help you manage multiple lists of things.\n");
-		helpMessage.append("Use the /lists command to show all lists and to manage them.\n");
-		helpMessage.append("Use the /newlist command to create a new list.\n");
-		helpMessage.append("Use the /cancel command to ignore previous actions.\n");
-		helpMessage.append("Use the /help command to show this message.\n");
-		chat.sendMessage(helpMessage.toString());
+		String helpMessage = "This bot can help you manage multiple lists of things.\n"
+				+ "Use the /lists command to show all lists and to manage them.\n"
+				+ "Use the /newlist command to create a new list.\n"
+				+ "Use the /cancel command to ignore previous actions.\n"
+				+ "Use the /help command to show this message.\n";
+		chat.sendMessage(helpMessage);
 	}
 }
